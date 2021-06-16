31b1d20546b94c9808fafe2de4299bdf9dfcdd6e
Otavio Augusto da Costa Pimentel <OtavioAugusto.daCostaPimentel-ext@ab-inbev.com>
2021-06-16 16:39:00 -0300

    Added chain promises

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -17,7 +17,16 @@ export function getCatch() {
     });
 }
 
-export function chain() {}
+export function chain() {
+  axios
+    .get("http://localhost:3000/orders/1")
+    .then(({ data }) => {
+      return axios.get(
+        `http://localhost:3000/addresses/${data.shippingAddress}`
+      );
+    })
+    .then(({ data }) => setText(`City: ${data.city}`));
+}
 
 export function chainCatch() {}
 export function final() {}
