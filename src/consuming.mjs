38a4034bde2d47ae10d183d107d807968f498693
Otavio Augusto da Costa Pimentel <OtavioAugusto.daCostaPimentel-ext@ab-inbev.com>
2021-06-16 16:54:04 -0300

    Added chain catches and explanations

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -28,5 +28,22 @@ export function chain() {
     .then(({ data }) => setText(`City: ${data.city}`));
 }
 
-export function chainCatch() {}
+export function chainCatch() {
+  axios
+    .get("http://localhost:3000/orders/1")
+    .then(({ data }) => {
+      return axios.get(
+        `http://localhost:3000/addresses/${data.shippingAddress}`
+      );
+    })
+    // This 'catch' block will only work for the first 'then' and after that, he return an object because if didn't, the second 'then'
+    // will throw another error, because the 'data' object will be undefined and they cannot search fro 'data.my.city'
+    // .catch((error) => {
+    //   setText(error);
+    //   return { data: {} };
+    // })
+    .then(({ data }) => setText(`City: ${data.my.city}`))
+    //this 'catch' will work with the two 'then' above him, so, if one of then fails, will end at this catch block
+    .catch((error) => setText(error));
+}
 export function final() {}
