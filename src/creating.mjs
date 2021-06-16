dea5f3a11a276df72ff841b5ac50a19b149d0be5
Otavio Augusto da Costa Pimentel <OtavioAugusto.daCostaPimentel-ext@ab-inbev.com>
2021-06-16 18:04:48 -0300

    Added interval and clearInterval

--- src/creating.mjs
+++ src/creating.mjs
@@ -1,22 +1,45 @@
 import setText, { appendText } from "./results.mjs";
 
 export function timeout() {
+  const wait = new Promise((resolve) => {
+    setTimeout(() => resolve("TimeOut!!!"), 1500);
+  });
+
+  wait.then((text) => setText(text));
 }
 
 export function interval() {
+  const wait = new Promise((resolve) => {
+    setInterval(() => {
+      console.log("Time");
+      resolve("TimeOut!!!");
+    }, 1500);
+  });
+
+  wait.then((text) => setText(text));
 }
 
 export function clearIntervalChain() {
+  let interval;
+
+  const wait = new Promise((resolve) => {
+    interval = setInterval(() => {
+      console.log("Time");
+      resolve("Interval");
+    }, 1000);
+  });
+
+  wait.then((text) => {
+    setText(text);
+    appendText(" -- Finished --");
+    clearInterval(interval);
+  });
 }
 
-export function xhr() {
-}
+export function xhr() {}
 
-export function allPromises() {
-}
+export function allPromises() {}
 
-export function allSettled() {
-}
+export function allSettled() {}
 
-export function race() {
-}
+export function race() {}
