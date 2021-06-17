e42ddd4775cfc8039fdb23e4fc8b02c78530fdc5
Otavio Augusto da Costa Pimentel <OtavioAugusto.daCostaPimentel-ext@ab-inbev.com>
2021-06-17 20:21:40 -0300

    Added Async/Await codes

--- src/iterating.mjs
+++ src/iterating.mjs
@@ -1,16 +1,53 @@
-import setText , {appendText} from './results.mjs';
+import setText, { appendText } from "./results.mjs";
 
-export function get(){
+export async function get() {
+  const { data } = await axios.get("http://localhost:3000/orders/1");
+  setText(JSON.stringify(data));
 }
 
-export function getCatch(){
+export async function getCatch() {
+  try {
+    const { data } = await axios.get("http://localhost:3000/orders/123");
+    setText(JSON.stringify(data));
+  } catch (e) {
+    setText(e);
+  }
 }
 
-export function chain(){
+export async function chain() {
+  const { data } = await axios.get("http://localhost:3000/orders/1");
+  const { data: address } = await axios.get(
+    `http://localhost:3000/addresses/${data.shippingAddress}`
+  );
+  setText(`City: ${address.city}`);
 }
 
-export function concurrent(){
+export async function concurrent() {
+  //here, since promises are eager evaluated, when we define them, they already start the request
+  const orderStatus = axios.get("http://localhost:3000/orderStatuses");
+  const orders = axios.get("http://localhost:3000/orders");
+
+  setText("");
+
+  //here we are forcing the code to threat each data at time, regardless of wich request finishes first
+  const { data: statuses } = await orderStatus;
+  const { data: order } = await orders;
+
+  appendText("1 -" + JSON.stringify(statuses) + "-------");
+  appendText(JSON.stringify(order[0]));
 }
 
-export function parallel(){
+export async function parallel() {
+  setText("");
+
+  await Promise.all([
+    (async () => {
+      const { data } = await axios.get("http://localhost:3000/orderStatuses");
+      appendText("--1--" + JSON.stringify(data) + "--1--");
+    })(),
+    (async () => {
+      const { data } = await axios.get("http://localhost:3000/orders");
+      appendText(JSON.stringify(data[0]));
+    })(),
+  ]);
 }
